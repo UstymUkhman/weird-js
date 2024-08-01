1bb3ec068885eb3f43dbd6a69e2802f997d856c4
Ustym Ukhman <ustym.ukhman@gmail.com>
2024-08-01 09:35:45 +0300

    Repository Initialized

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-clear
-echo ""
-zig build
-./zig-out/bin/weird-js.exe # $1 $2
--- vite.config.js
+++
@@ -1,6 +0,0 @@
-import zigar from "rollup-plugin-zigar";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [zigar({ topLevelAwait: false })]
-});
